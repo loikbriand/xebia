980cd4ab8c170a2253b421c4c204ab89c9ee5779
kiolol <loik.briand@gmail.com>
2018-07-18 19:59:02 +0200

    ansible test

--- war/src/main/java/fr/xebia/clickcount/Configuration.java
+++ war/src/main/java/fr/xebia/clickcount/Configuration.java
@@ -10,7 +10,7 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = "redis";
+        redisHost = "18.196.88.85";
         redisPort = 6379;
         redisConnectionTimeout = 2000;
     }
