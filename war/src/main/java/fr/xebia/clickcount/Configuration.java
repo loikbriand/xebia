cdb0f313daa6000d86c76bb1abfb6643ce332517
kiolol <loik.briand@gmail.com>
2018-07-21 15:32:22 +0200

    ajout du chargement des properties

--- war/src/main/java/fr/xebia/clickcount/Configuration.java
+++ war/src/main/java/fr/xebia/clickcount/Configuration.java
@@ -2,6 +2,11 @@ package fr.xebia.clickcount;
 
 import javax.inject.Singleton;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @Singleton
 public class Configuration {
 
@@ -10,8 +15,36 @@ public class Configuration {
     public final int redisConnectionTimeout;  //milliseconds
 
     public Configuration() {
-        redisHost = "18.196.88.85";
-        redisPort = 6379;
+
         redisConnectionTimeout = 2000;
+
+        String redisHostProp = "redis";
+        int redisPortProp = 1234;
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(System.getProperty("catalina.home") + "/properties/redis.properties");
+            prop.load(input);
+
+            redisHostProp = prop.getProperty("server.redis.host");
+            redisPortProp = Integer.parseInt(prop.getProperty("server.redis.port"));
+
+        } catch (final IOException ex) {
+            ex.printStackTrace();
+        } finally {
+
+            redisHost = redisHostProp;
+            redisPort = redisPortProp;
+
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
