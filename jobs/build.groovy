f543d90faf432307df432a13abad6b0d2fe09290
kiolol <loik.briand@gmail.com>
2018-07-21 19:10:34 +0200

    ansible test

--- jobs/build.groovy
+++ jobs/build.groovy
@@ -1,15 +1,9 @@
 pipelineJob('build clickCount') {
     definition {
-        cps {
-            script('''
-stage "build"
-node {
-    deleteDir()
-    git "https://github.com/loikbriand/xebia.git"
-    sh "mvn clean deploy"
-}
-''')
-            sandbox()
+        cpsScm {
+            scm {
+                git('https://github.com/loikbriand/xebia.git')
+            }
         }
     }
 }
